ec33ad368348d998d9fd2de09ba57f99f7e0f2fb
Zulko <valentin.zulkower@gmail.com>
2016-12-15 17:45:09 +0000

    fixed single_assembly example

--- examples/single_assembly.py
+++ examples/single_assembly.py
@@ -1,9 +1,6 @@
 from dnacauldron import single_assembly
 import os
 os.chdir("data")
-single_assembly(
-    parts_filenames=["partA.gb", "partB.gb", "partC.gb"],
-    receptor_filename="receptor.gb",
-    outfile=os.path.join("..", "final_sequence.gb"),
-    enzyme="BsmBI"
-)
+single_assembly(parts=["partA.gb", "partB.gb", "partC.gb"],
+                receptor="receptor.gb", enzyme="BsmBI",
+                outfile=os.path.join("..", "final_sequence.gb"))
