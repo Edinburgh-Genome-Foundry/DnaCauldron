4ea8873905e90010b2d87c38a07d33c97de8b75c
Zulko <valentin.zulkower@gmail.com>
2018-02-16 18:41:07 +0000

    version 0.1.3

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ exec(open('dnacauldron/version.py').read()) # loads __version__
 setup(name='dnacauldron',
       version=__version__,
       author='Zulko',
-    description='',
+    url='https://github.com/Edinburgh-Genome-Foundry/DnaCauldron',
+    description='DNA cloning simulation for restriction based assembly and more',
     long_description=open('pypi-readme.rst').read(),
     license='see LICENSE.txt',
-    keywords="",
+    keywords="DNA cloning simulator restriction assembly",
     packages= find_packages(exclude='docs'),
     install_requires=['Biopython', 'numpy', 'matplotlib', 'pandas', 'scipy',
                       'networkx', 'dna_features_viewer', 'flametree'])
