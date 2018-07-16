479923f10bc5e56c53b3a34afbc441504d96e000
Zulko <valentin.zulkower@gmail.com>
2018-07-16 17:31:43 +0100

    added xlwt as a dependency

--- setup.py
+++ setup.py
@@ -16,4 +16,4 @@ setup(name='dnacauldron',
     packages= find_packages(exclude='docs'),
     install_requires=['Biopython', 'numpy', 'matplotlib', 'pandas', 'scipy',
                       'networkx', 'dna_features_viewer', 'flametree',
-                      'snapgene_reader', 'proglog'])
+                      'snapgene_reader', 'proglog', 'xlwt'])
