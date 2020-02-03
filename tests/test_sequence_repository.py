7fb08666052da2145cb107277ef2cdd14c5bb19c
Zulko <valentin.zulkower@gmail.com>
2020-02-03 14:02:09 +0000

    new test for repo_part_names_by_collection

--- tests/test_sequence_repository.py
+++ tests/test_sequence_repository.py
@@ -16,3 +16,11 @@ def test_not_in_repository_error():
     repo.add_records(records)
     with pytest.raises(dc.NotInRepositoryError):
         repo.get_records(['id_3', 'id_4'])
+
+def test_get_repo_part_names_by_collection():
+    repo = dc.SequenceRepository()
+    repo.add_records([("id_1", "ATGC")], collection="parts")
+    repo.add_records([("id_2", "ATGC")], collection="other")
+    
+    result = repo.get_part_names_by_collection(format="string")
+    assert result == 'parts\n- id_1\nother\n- id_2'
