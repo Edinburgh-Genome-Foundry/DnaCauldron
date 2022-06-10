81d06a0167134d93678a44b83c32def746370ea8
Peter Vegh <pvegh@posteo.uk>
2022-06-10 22:42:50 +0100

    Test StickyEndSeq.slice_seq()

--- tests/test_fragments/test_fragments.py
+++ tests/test_fragments/test_fragments.py
@@ -29,6 +29,21 @@ def test_StickyEndSeq():
     )
     assert sticky.__repr__() == "(ATCG(+)-aaaat(16)cgggg-ATCG(-))"
 
+    # Test slice_seq()
+    sticky = StickyEndSeq(
+        Seq("AAAATTTTCCCCGGGG"),
+        left_end=StickyEnd("ATCG", strand=+1),
+        right_end=StickyEnd("ATCG", strand=-1),
+    )
+    sliced_sticky = sticky.slice_seq(start=0, end=4)
+    assert sliced_sticky.__repr__() == "(None-AAAA-None)"
+
+    sliced_sticky = sticky.slice_seq(start=12)
+    assert sliced_sticky.__repr__() == "(None-GGGG-None)"
+
+    sliced_sticky = sticky.slice_seq(end=12)
+    assert sliced_sticky.__repr__() == "(None-AAAATTTTCCCC-None)"
+
 
 def test_StickyEndSeqFragment():
     sticky = StickyEndSeq(
